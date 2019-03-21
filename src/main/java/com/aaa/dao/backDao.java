a8fe655348884371896b32e635f6dbd4ebac12cc
grantemily <wl1870971483@163.com>
2019-03-21 16:11:12 +0800

    后台模板

--- src/main/java/com/aaa/dao/backDao.java
+++ src/main/java/com/aaa/dao/backDao.java
@@ -1,7 +1,12 @@
 package com.aaa.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.aaa.entity.tadmin;
 
@@ -10,4 +15,16 @@ public interface backDao {
 	//管理员个人查询
 	@Select("select * from tadmin where tid = #{param1}")
 	public tadmin query(Integer tid);
+	//查询所有管理员
+	@Select("select * from tadmin")
+	public List<tadmin> queryTadmin();
+	//修改自己的信息（自己为管理员）
+	@Update("update tadmin set tname=#{tname},tpwd=#{tpwd} where tid = #{tid}")
+	public Integer editMyself(tadmin t);
+	//删除管理员
+	@Delete("delete from tadmin where tid = #{param1}")
+	public Integer delAdmin(Integer tid);
+	//添加一个管理员
+	@Insert("insert into tadmin(tname,tpwd) values(#{tname},#{tpwd})")
+	public Integer addAdmin(tadmin t);
 }
